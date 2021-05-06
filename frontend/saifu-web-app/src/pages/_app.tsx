adf2642419b4adff23a25768ddc1cd63a64ccf24
JojiKoike <joji.koike@gmail.com>
2021-05-06 23:02:45 +0900

    :+1: Matefial-UI for SSR

--- frontend/saifu-web-app/src/pages/_app.tsx
+++ frontend/saifu-web-app/src/pages/_app.tsx
@@ -1,7 +1,35 @@
+import React, {useEffect} from 'react';
 import type {AppProps} from 'next/app';
 
-function MyApp({Component, pageProps}: AppProps): React.ReactNode {
-  return <Component {...pageProps} />;
+import {ThemeProvider as StyledComponentsThemeProvider} from 'styled-components';
+import {
+  ThemeProvider as MaterialUIThemeProvider,
+  StylesProvider,
+} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import theme from '../styles/theme';
+
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+  useEffect(() => {
+    const jssStyles: Element | null = document.querySelector(
+      '#jss-server-side',
+    );
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </StyledComponentsThemeProvider>
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  );
 }
 
 export default MyApp;
