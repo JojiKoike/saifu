084901e6120be7ae437d9eabb02f418d4ec8ff56
JojiKoike <joji.koike@gmail.com>
2021-05-06 18:39:08 +0900

    :recycle: Restructure Directory

--- frontend/saifu-web-app/src/pages/_app.tsx
+++ frontend/saifu-web-app/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type {AppProps} from 'next/app';
-import '../styles/global.scss';
 
 function MyApp({Component, pageProps}: AppProps): React.ReactNode {
   return <Component {...pageProps} />;
