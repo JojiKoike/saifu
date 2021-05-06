bd8a407dff5dde1f34aec69748f2482d079876eb
JojiKoike <joji.koike@gmail.com>
2021-05-06 18:08:07 +0900

    :+1: Temporary Commit

--- frontend/saifu-web-app/.storybook/main.js
+++ frontend/saifu-web-app/.storybook/main.js
@@ -1,11 +1,7 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
-  ]
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
