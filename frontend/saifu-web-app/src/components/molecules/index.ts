00f70eb4b263f78c43d81710e4ba0fbc248e8eca
JojiKoike <joji.koike@gmail.com>
2021-05-16 13:34:09 +0900

    :+1: Molecules Checkboxes Implemented

--- frontend/saifu-web-app/src/components/molecules/index.ts
+++ frontend/saifu-web-app/src/components/molecules/index.ts
@@ -4,3 +4,5 @@ export {default as AppBar} from './appBar/AppBar';
 export type {AppBarProps} from './appBar/AppBar';
 export {default as Footer} from './footer/Footer';
 export type {FooterProps} from './footer/Footer';
+export {default as Checkboxes} from './checkboxes';
+export type {CheckboxesProps} from './checkboxes';
