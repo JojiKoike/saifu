9717131e07bca5878d48bc87876e42cc5005fd9b
JojiKoike <joji.koike@gmail.com>
2021-05-23 10:32:43 +0900

    :green_heart: Storybook Decorator For Material-ui and styled-components

--- frontend/saifu-web-app/.storybook/preview.js
+++ frontend/saifu-web-app/.storybook/preview.js
@@ -1,9 +1,25 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, StylesProvider } from '@material-ui/core/styles';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/styles/theme';
+
+
 export const parameters = {
-  actions: {argTypesRegex: '^on[A-Z].*'},
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Story {...context} />
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
+  )
+}
+
+export const decorators = [withThemeProvider]
