ecf6ace4700110389f2fc490a1ef28e9e4df4042
JojiKoike <joji.koike@gmail.com>
2021-05-24 00:18:35 +0900

    :sparkles: Theme Difinision

--- frontend/saifu-web-app/src/styles/theme.ts
+++ frontend/saifu-web-app/src/styles/theme.ts
@@ -1,3 +1,19 @@
 import {createMuiTheme} from '@material-ui/core';
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#03A9F4',
+      dark: '#0288D1',
+      light: '#B3E5FC',
+    },
+    secondary: {
+      main: '#FF5252',
+    },
+    text: {
+      primary: '#212121',
+      secondary: '#757575',
+    },
+    divider: '#BDBDBD',
+  },
+});
 export default theme;
